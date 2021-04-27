21450b02e0b4afea7b7e0ca46515c56f31ca8c48
Azamat <azamatkarakol92@gmail.com>
2021-04-28 01:41:26 +0400

    initial

---
+++ js/carousel.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.cards').slick({
+        slidesToShow: 3,
+
+    });
+});
+
+$(document).ready(function () {
+    $('.clients__cards').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+});
